70a12088232593682ac914bc2bcb92b8e19bed52
durgaprasad773 <131637821+durgaprasad773@users.noreply.github.com>
2023-10-21 08:14:08 +0530

    main.py

--- Deloitte/main.py
+++ Deloitte/main.py
@@ -1,50 +1,102 @@
-import json
-from datetime import datetime
+import json, unittest, datetime
 
-def convert_iso_to_milliseconds(iso_timestamp):
-    """
-    Convert an ISO timestamp to milliseconds timestamp.
-    
-    Args:
-        iso_timestamp (str): The ISO format timestamp.
-        
-    Returns:
-        int: The timestamp in milliseconds.
-    """
-    # Parse the ISO timestamp to a datetime object
-    iso_datetime = datetime.fromisoformat(iso_timestamp)
-    
-    # Convert the datetime object to a Unix timestamp in milliseconds
-    milliseconds_timestamp = int(iso_datetime.timestamp() * 1000)
-    
-    return milliseconds_timestamp
+with open("./data-1.json","r") as f:
+    jsonData1 = json.load(f)
+with open("./data-2.json","r") as f:
+    jsonData2 = json.load(f)
+with open("./data-result.json","r") as f:
+    jsonExpectedResult = json.load(f)
 
-def convert_data(data):
-    """
-    Convert data from ISO format to milliseconds format.
-    
-    Args:
-        data (list): A list of data entries in ISO format.
-        
-    Returns:
-        list: A list of data entries with timestamps in milliseconds format.
-    """
-    converted_data = []
-    
-    for entry in data:
-        entry['timestamp'] = convert_iso_to_milliseconds(entry['timestamp'])
-        converted_data.append(entry)
-    
-    return converted_data
 
-# Read the input data from data-1.json
-with open("data-1.json", "r") as file:
-    data_1 = json.load(file)
+def convertFromFormat1 (jsonObject):
 
-# Convert the data to milliseconds format
-converted_data_1 = convert_data(data_1)
+    locationParts = jsonObject['location'].split('/')
 
-# Save the converted data to data-result.json
-with open("data-result.json", "w") as file:
-    json.dump(converted_data_1, file, indent=2)
+    result = {
+        'deviceID': jsonObject['deviceID'],
+        'deviceType': jsonObject['deviceType'],
+        'timestamp': jsonObject['timestamp'],
+        'location': {
+            'country': locationParts[0],
+            'city': locationParts[1],
+            'area': locationParts[2],
+            'factory': locationParts[3],
+            'section': locationParts[4]
+        },
+        'data': {
+            'status': jsonObject['operationStatus'],
+            'temperature': jsonObject['temp']
+        }
+    }
 
+    return result
+
+def convertFromFormat2 (jsonObject):
+
+    date = datetime.datetime.strptime(
+        jsonObject['timestamp'],
+        '%Y-%m-%dT%H:%M:%S.%fZ'
+    )
+    timestamp = round(
+        (date - datetime.datetime(1970, 1, 1)).total_seconds() * 1000
+    )
+
+    result = {
+        'deviceID': jsonObject['device']['id'],
+        'deviceType': jsonObject['device']['type'],
+        'timestamp': timestamp,
+        'location': {
+            'country': jsonObject['country'],
+            'city': jsonObject['city'],
+            'area': jsonObject['area'],
+            'factory': jsonObject['factory'],
+            'section': jsonObject['section']
+        },
+        'data': jsonObject['data']
+    }
+
+    return result
+
+
+def main (jsonObject):
+
+    result = {}
+
+    if (jsonObject.get('device') == None):
+        result = convertFromFormat1(jsonObject)
+    else:
+        result = convertFromFormat2(jsonObject)
+
+    return result
+
+
+class TestSolution(unittest.TestCase):
+
+    def test_sanity(self):
+
+        result = json.loads(json.dumps(jsonExpectedResult))
+        self.assertEqual(
+            result,
+            jsonExpectedResult
+        )
+
+    def test_dataType1(self):
+
+        result = main (jsonData1)
+        self.assertEqual(
+            result,
+            jsonExpectedResult,
+            'Converting from Type 1 failed'
+        )
+
+    def test_dataType2(self):
+
+        result = main (jsonData2)
+        self.assertEqual(
+            result,
+            jsonExpectedResult,
+            'Converting from Type 2 failed'
+        )
+
+if __name__ == '__main__':
+    unittest.main()
